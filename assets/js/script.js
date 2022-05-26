890ae62d1ed67fb8151d4d7198d9d3c46991257c
nComin <na_comin@hotmail.com>
2022-05-26 18:26:37 -0400

    Inclusao do desafio do contador usando JavaScript

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+let count = 0;
+
+const CURRENT_NUMBER = document.getElementById('currentNumber');
+
+CURRENT_NUMBER.addEventListener("mouseover", function() {
+	alert("thank you for being here");
+}, false);
+
+function increment() {
+	count++;
+	CURRENT_NUMBER.innerHTML = count;
+}
+
+function decrement() {
+	count--;
+	CURRENT_NUMBER.innerHTML = count;
+}
+
+function test() {
+	kdowkdpo;
+}
+
+
